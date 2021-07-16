57487731af633274e8dfb02c7ec0bf250bed3019
KelvinIO <kelviinvn@protonmail.com>
2021-07-16 18:35:45 +1000

    Fixed formatting of the sudoku grid on the drawSudoku() function

--- main.cpp
+++ main.cpp
@@ -5,9 +5,11 @@ void drawSudoku(int grid[9][9]){
   std::cout << "------------------" << std::endl;
   
   for (int row = 0; row < 9; row++){ // For every row...
-    for (int col = 0; col < 9; col++){ // and every coloumn
-      std::cout << "| " << grid[row][col] << " |" << std::endl;
+      std::cout << "|";
+    for (int col = 0; col < 9; col++){ // ...and every column
+      std::cout << " " << grid[row][col] << " |"; // ...print its value
     }
+      std::cout << std::endl;
   }
 
   std::cout << "------------------" << std::endl;
